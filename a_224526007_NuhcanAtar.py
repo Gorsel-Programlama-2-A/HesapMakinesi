831a085ee46794ca128e1136350a6c23bbf47a01
Nuhcan <nuhcanatar20@gmail.com>
2023-11-13 17:58:26 +0300

    Update a_224526007_NuhcanAtar.py

--- a_224526007_NuhcanAtar.py
+++ a_224526007_NuhcanAtar.py
@@ -1,136 +1,98 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 31 10:38:47 2023
-
-@author: Excalibur
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 31 08:34:01 2023
-
-@author: Excalibur
-"""
-
-
-def c():
-    inputValue.delete(0,tk.END)
-    valueSonuc.delete(0,tk.END)
-
-def topla():
-    try:
-        sayi = float(inputValue.get())
-        sonuc_text = float(valueSonuc.get())
-        yeni_sonuc = sayi + sonuc_text
-        valueSonuc.delete(0, tk.END)
-        valueSonuc.insert(0, yeni_sonuc)        
-    except ValueError:
-        messagebox.showerror("Hata", "Geçerli bir sayı girin.")
-        
-def carpma():
-    try:
-        sayi = float(inputValue.get())
-        sonuc_text = float(valueSonuc.get())
-        yeni_sonuc = sayi * sonuc_text
-        valueSonuc.delete(0, tk.END)
-        valueSonuc.insert(0, yeni_sonuc)        
-    except ValueError:
-        messagebox.showerror("Hata", "Geçerli bir sayı girin.")
-        
-def bolme():
-    try:
-        sayi = float(inputValue.get())
-        sonuc_text = float(valueSonuc.get())
-        yeni_sonuc = sayi / sonuc_text
-        valueSonuc.delete(0, tk.END)
-        valueSonuc.insert(0, yeni_sonuc)        
-    except ValueError:
-        messagebox.showerror("Hata", "Geçerli bir sayı girin.")
-        
-def cikarma():
-    try:
-        sayi = float(inputValue.get())
-        sonuc_text = float(valueSonuc.get())
-        yeni_sonuc = sayi - sonuc_text
-        valueSonuc.delete(0, tk.END)
-        valueSonuc.insert(0, yeni_sonuc)        
-    except ValueError:
-        messagebox.showerror("Hata", "Geçerli bir sayı girin.")
-        
-def karekok():
-    try:
-        sayi = float(inputValue.get())
-        sonuc_text = float(valueSonuc.get())
-        yeni_sonuc = math.sqrt(sayi)
-        valueSonuc.delete(0, tk.END)
-        valueSonuc.insert(0, yeni_sonuc)        
-    except ValueError:
-        messagebox.showerror("Hata", "Geçerli bir sayı girin.")
-
-def usAl():
-    try:
-        sayi = float(inputValue.get())
-        sonuc_text = float(valueSonuc.get())
-        
-        yeni_sonuc = math.pow(sayi, sonuc_text)
-        valueSonuc.delete(0,tk.END)
-        valueSonuc.insert(0, yeni_sonuc)
-    except ValueError:
-        messagebox.showerror("Hata", "Değeri Giriniz")
-
+from tkinter import *
+from tkinter import ttk
 import math
-import tkinter as tk
-from tkinter import messagebox
 
-pencere = tk.Tk()
-pencere.title("Hesap Makinası Python")
-pencere.geometry("400x500")
+memory = 0
+
+def buttonHesapla(secilisembol):
+    input_text = input_widget.get()
+    
+    if(secilisembol) == "=":
+        try:
+            sonuc = eval(input_text)
+            input_widget.delete(0, 'end')
+            input_widget.insert('end', str(sonuc))
+        except Exception as e:
+            input_widget.delete(0, 'end')
+            input_widget.insert('end', 'Hata')
+    elif secilisembol == "C":
+        input_widget.delete(0, 'end')
+    elif secilisembol == "√":
+        input_widget.delete(0,'end')
+        input_widget.insert('end', str(math.sqrt(float(input_text))))
+    elif secilisembol == "^":
+        input_widget.delete(0,'end')
+        input_widget.insert('end', input_text + "**")
+    elif secilisembol == "M+":
+        
+        bellekEkle(input_text)
+        
+    elif secilisembol == "M-":
+        
+        bellekAzalt(input_text)
+        
+    elif secilisembol == "MC":
+        
+        bellegitamamensil()
+        
+    elif secilisembol == "MR":
+        
+        bellegicagir()
+    else:
+        input_widget.delete(0,'end')
+        input_widget.insert('end', input_text + str(secilisembol))
+    
+def bellekEkle(value):
+    global memory
+    try:
+        memory+= eval(value)
+    except Exception as e:
+        input_widget.delete(0, 'end')
+        input_widget.insert('end', "Hata")
+
+def bellekAzalt(value):
+    global memory
+    try:
+        memory-=eval(value)
+    except Exception as e:
+        input_widget.delete(0,'end')
+        input_widget.insert('end', "Hata")
+        
+def bellegitamamensil():
+    global memory
+    memory = 0
+    
+
+def bellegicagir():
+    global memory
+    input_widget.delete(0, 'end')
+    input_widget.insert('end', str(memory))    
+        
+
+anaform = Tk()
+
+anaform.title("Hesap Makinası")
+anaform.geometry("400x400")
 
 
-inputValue = tk.Entry(width=30)
-inputValue.grid(row=0, column=1, padx=(0, 10), pady=10, ipady=10, ipadx=0)  
-
-valueSonucText = tk.Label(text="Sonuç:")
-valueSonucText.grid(row=1, column=0, padx=10, pady=10)
-
-valueSonuc = tk.Entry(width=30,textvariable="0")
-valueSonuc.grid(row=1, column=1, padx=(0, 10), pady=10, ipady=10, ipadx=0) 
+# inputlar
+input_widget = Entry(anaform, width=30, font=('Arial', '16'))
+input_widget.grid(row=0, column=0, columnspan=4,padx=5,pady=5)
 
 
-
-toplama_button = tk.Button(text="Topla", width=10, height=2,command=topla)
-toplama_button.grid(row=2, column=0, padx=5, pady=5)
-
-cikarma_button = tk.Button(text="Çıkarma", width=10, height=2,command=cikarma)
-cikarma_button.grid(row=2, column=1, padx=5, pady=5)
-
-carpma_button = tk.Button(text="Çarpma", width=10, height=2,command=carpma)
-carpma_button.grid(row=2, column=2, padx=5, pady=5)
-
-
-bolme_button = tk.Button(text="Bölme", width=10, height=2,command=bolme)
-bolme_button.grid(row=3, column=0, padx=5, pady=5)
-
-karekok_button = tk.Button(text="Karekök", width=10, height=2,command=karekok)
-karekok_button.grid(row=3, column=1, padx=5, pady=5)
-
-usAl_button = tk.Button(text="Üs Al", width=10, height=2,command=usAl)
-usAl_button.grid(row=3, column=2, padx=5, pady=5)
-
-#
-
-bolme_button = tk.Button(text="MC", width=10, height=2)
-bolme_button.grid(row=4, column=0, padx=5, pady=5)
-
-karekok_button = tk.Button(text="C", width=10, height=2,command=c)
-karekok_button.grid(row=4, column=1, padx=5, pady=5)
-
-karekok_button = tk.Button(text="Üs Alma", width=10, height=2)
-karekok_button.grid(row=4, column=2, padx=5, pady=5)
+# butonlar listesi
+buttons = [
+    ('C', 1, 0), ('M+', 1, 1), ('M-', 1, 2), ('MC', 1, 3),
+    ('MR', 2, 0), ('√', 2, 1), ('^', 2, 2), ('/', 2, 3),
+    ('7', 3, 0), ('8', 3, 1), ('9', 3, 2), ('*', 3, 3),
+    ('4', 4, 0), ('5', 4, 1), ('6', 4, 2), ('-', 4, 3),
+    ('1', 5, 0), ('2', 5, 1), ('3', 5, 2), ('+', 5, 3),
+    ('0', 6, 0), ('=', 6, 1)
+    ]
 
 
+for (text, row, column) in buttons:
+    button_widget = ttk.Button(anaform, text=text, width=6, command=lambda t=text: buttonHesapla(t))
+    button_widget.grid(row=row,column=column, padx=5,pady=5)
 
-
-
-
-pencere.mainloop()
+anaform.mainloop()
